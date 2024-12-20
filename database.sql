c0eb6b2003e1032bebb81af6f52b3fa5e7c65b63
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-12-20 21:44:24 +0500

    Пересоздание таблиц если существуют

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
     id SERIAL PRIMARY KEY,
     url_id INTEGER NOT NULL,
     status_code INTEGER,
